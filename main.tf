de802b34667525ccce4932c67cd880e42a0b7331
itsabhi06 <abhiishekabhi06@gmail.com>
2025-06-25 03:59:09 +0530

    fix: updated keyname for ec2 instance

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "ec2" {
   source    = "./modules/ec2"
   vpc_id    = module.vpc.vpc_id
   subnet_id = module.vpc.public_subnet_id
-  key_name  = var.key_name
+  key_name  = "web-instance-key"
 }
 
 module "s3" {

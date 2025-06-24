b37164cba10f1493a14741787e6b8de77e60d123
itsabhi06 <abhiishekabhi06@gmail.com>
2025-06-25 03:37:53 +0530

    Added SSH key support and security group for EC2

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -7,3 +7,8 @@ variable "subnet_id" {
   description = "Subnet ID to launch EC2 instance"
   type        = string
 }
+
+variable "key_name" {
+  description = "SSH key pair name for EC2 login"
+  type        = string
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   description = "aws region"
   default     = "us-east-1"
 }
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
